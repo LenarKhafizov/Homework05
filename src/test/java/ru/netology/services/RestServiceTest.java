135bc84ca9d7fe8a471107fa4c51b9b01c9913f6
Lenar Khafizov <lenarkhaf@gmail.com>
2025-03-10 20:14:10 +0300

    Промежуточное выполнение задания 2

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -1,34 +1,14 @@
 package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestServiceTest {
-    @Test
-    public void shouldCalculateCaseOne() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+
+    public void shouldCalculateCaseOne(int income, int expenses, int threshold, int expected) {
         RestService service = new RestService();
-
-        // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold  = 20_000;
-        int expected = 3;
-
-        // вызываем целевой метод:
-        int actual = service.calculate(income, expenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateCaseTwo() {
-        RestService service = new RestService();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold  = 150_000;
-        int expected = 2;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
